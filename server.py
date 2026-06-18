ba9a4ce24908e29ba9ed3867cb1420752211cf4d
Adam G. <adamgryc@aol.de>
2026-06-18 09:51:35 +0200

    run with uvicorn rather than with mcp

--- server.py
+++ server.py
@@ -1,12 +1,17 @@
+print("🚀 MCP server starting...")
+
 from mcp.server.fastmcp import FastMCP
+import uvicorn
+import os
 
 mcp = FastMCP("age-server")
-
+app = mcp.sse_app()
 
 @mcp.tool()
 def get_age_by_name(name: str) -> int:
     return {"Hans": 52, "Peter": 28}.get(name, -1)
 
-
 if __name__ == "__main__":
-    mcp.run(transport="sse")
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("server:app", host="0.0.0.0", port=port)
+
