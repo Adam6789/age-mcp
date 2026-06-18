4f235b5dba708322b598250a4ea5f143233c0491
Adam G. <adamgryc@aol.de>
2026-06-18 10:22:56 +0200

    add middleware

--- server.py
+++ server.py
@@ -1,11 +1,18 @@
-print("🚀 MCP server starting...")
-
+from starlette.middleware.trustedhost import TrustedHostMiddleware
 from mcp.server.fastmcp import FastMCP
 import uvicorn
 import os
 
+print("🚀 MCP server starting...")
 mcp = FastMCP("age-server")
 app = mcp.streamable_http_app()
+
+app.add_middleware(TrustedHostMiddleware, allowed_hosts=[
+    "age-mcp.onrender.com",
+    "*.onrender.com",
+    "localhost",
+    "127.0.0.1",
+], )
 
 @mcp.tool()
 def get_age_by_name(name: str) -> int:
